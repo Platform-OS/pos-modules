fdf4c279651a4155668f7038c595bf264b42ee3e
Rafal Krysiak <rafal@near-me.com>
2026-04-08 18:06:30 +0200

    Remove unneeded files and modify seed.sh accordingly

--- pos-module-payments-stripe/tests/data/seed/seed.sh
+++ pos-module-payments-stripe/tests/data/seed/seed.sh
@@ -3,9 +3,10 @@ set -eu
 DEFAULT_ENV=""
 POS_ENV="${1:-$DEFAULT_ENV}"
 
+mkdir -p app/
+# This also installs core module
+pos-cli modules install payments
+
 pos-cli data clean $POS_ENV --auto-confirm --include-schema
-
-cd ./tests/post_import
-
-env CONFIG_FILE_PATH=./../../.pos pos-cli deploy $POS_ENV
-env CONFIG_FILE_PATH=./../../.pos pos-cli constants set  --name stripe_sk_key --value $STRIPE_SK_KEY $POS_ENV
+pos-cli deploy $POS_ENV
+pos-cli constants set  --name stripe_sk_key --value $STRIPE_SK_KEY $POS_ENV
